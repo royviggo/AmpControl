cd8deff56db91a4e78c4de5c59b7fa112eceb966
royviggo <royviggo@gmail.com>
2019-06-11 22:33:32 +0200

    Button now returns state, display it

--- src/AmpControl/AmpControl.ino
+++ src/AmpControl/AmpControl.ino
@@ -8,26 +8,26 @@ const int AMPC_LCD_ROWS = 2;
 const int AMPC_LCD_CHARSIZE = 0;
 
 // Arduino pins for the LCD display
-const int AMPC_LCD_PIN_RS = 8;
-const int AMPC_LCD_PIN_EN = 9;
-const int AMPC_LCD_PIN_D4 = 10;
-const int AMPC_LCD_PIN_D5 = 11;
-const int AMPC_LCD_PIN_D6 = 12;
-const int AMPC_LCD_PIN_D7 = 13;
+const int AMPC_LCD_PIN_RS = 7;
+const int AMPC_LCD_PIN_EN = 8;
+const int AMPC_LCD_PIN_D4 = 9;
+const int AMPC_LCD_PIN_D5 = 10;
+const int AMPC_LCD_PIN_D6 = 11;
+const int AMPC_LCD_PIN_D7 = 12;
 
 // Button pins
 const int AMPC_BUTTON_LEFT = 2;
 const int AMPC_BUTTON_RIGHT = 3;
 const int AMPC_BUTTON_SELECT = 4;
-const int AMPC_BUTTON_MODE = INPUT_PULLUP;
+const int AMPC_BUTTON_SELECT_LONG = true;
+const int AMPC_BUTTON_ACTIVE = false;
 
 // Global vaiables
 LcdDisplay display = LcdDisplay(AMPC_LCD_COLS, AMPC_LCD_ROWS, AMPC_LCD_CHARSIZE);
-int counter = 0;
 
-Button leftBtn = Button(AMPC_BUTTON_LEFT, AMPC_BUTTON_MODE, &leftBtnNormalClick);
-Button rightBtn = Button(AMPC_BUTTON_RIGHT, AMPC_BUTTON_MODE, &rightBtnNormalClick);
-Button selectBtn = Button(AMPC_BUTTON_SELECT, AMPC_BUTTON_MODE, &selectBtnNormalClick, &selectBtnLongClick);
+Button leftBtn = Button(AMPC_BUTTON_LEFT, AMPC_BUTTON_ACTIVE);
+Button rightBtn = Button(AMPC_BUTTON_RIGHT, AMPC_BUTTON_ACTIVE);
+Button selectBtn = Button(AMPC_BUTTON_SELECT, AMPC_BUTTON_ACTIVE, AMPC_BUTTON_SELECT_LONG);
 
 void setup()
 {
@@ -35,33 +35,29 @@ void setup()
     display.print(0, 0, "Hello world!");
 
     Serial.begin(115200);
+    Serial.println("Setup Ok");
 }
 
 void loop()
 {
-    display.print(15, 0, String(++counter), false);
+    ButtonState leftBtnState = leftBtn.checkState();
+    if (leftBtnState == NORMAL_CLICK)
+        display.print(0, 1, "leftBtn!        ");
 
-    leftBtn.check();
-    rightBtn.check();
-    selectBtn.check();
-}
+    ButtonState rightBtnState = rightBtn.checkState();
+    if (rightBtnState == NORMAL_CLICK)
+        display.print(0, 1, "rightBtn!       ");
 
-void leftBtnNormalClick()
-{
-    Serial.println("Left button normal click");
-}
+    ButtonState selectBtnState = selectBtn.checkState();
+    if (selectBtnState == NORMAL_CLICK)
+        display.print(0, 1, "selectBtn!      ");
+    if (selectBtnState == LONG_CLICK)
+        display.print(0, 1, "selectBtn LONG! ");
 
-void rightBtnNormalClick()
-{
-    Serial.println("Right button normal click");
-}
-
-void selectBtnNormalClick()
-{
-    Serial.println("Select button normal click");
-}
-
-void selectBtnLongClick()
-{
-    Serial.println("Select button long click");
+    if (leftBtnState == NORMAL_CLICK || rightBtnState == NORMAL_CLICK ||
+        selectBtnState == NORMAL_CLICK || selectBtnState == LONG_CLICK)
+    {
+        delay(800);
+        display.print(0, 1, "                ");
+    }
 }
