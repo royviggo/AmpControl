81b2baadd4cea174060d656f30e4d9d025e4842a
Roy Viggo Pedersen <royviggo@gmail.com>
2020-05-19 22:01:54 +0200

    Fixed typo and formatting

--- src/AmpControl/LcdDisplay.cpp
+++ src/AmpControl/LcdDisplay.cpp
@@ -8,7 +8,8 @@ void LcdDisplay::begin(uint8_t pinRS, uint8_t pinEN, uint8_t pinD4, uint8_t pinD
     lcd.begin(cols, rows, charSize);
 }
 
-void LcdDisplay::begin(uint8_t pinRS, uint8_t pinEN, uint8_t pinD0, uint8_t pinD1, uint8_t pinD2, uint8_t pinD3, uint8_t pinD4, uint8_t pinD5, uint8_t pinD6, uint8_t pinD7)
+void LcdDisplay::begin(uint8_t pinRS, uint8_t pinEN, uint8_t pinD0, uint8_t pinD1, uint8_t pinD2, uint8_t pinD3, 
+                       uint8_t pinD4, uint8_t pinD5, uint8_t pinD6, uint8_t pinD7)
 {
     lcd = LiquidCrystal(pinRS, pinEN, pinD0, pinD1, pinD2, pinD3, pinD4, pinD5, pinD6, pinD7);
     lcd.begin(cols, rows, charSize);
@@ -16,12 +17,12 @@ void LcdDisplay::begin(uint8_t pinRS, uint8_t pinEN, uint8_t pinD0, uint8_t pinD
 
 void LcdDisplay::print(uint8_t posX, uint8_t posY, String text, bool leftAligned)
 {
-    uint8_t textLenght = text.length();
+    uint8_t textLength = text.length();
 
     if (!leftAligned)
-        posX = textLenght > posX ? 0 : posX - textLenght + 1;
+        posX = textLength > posX ? 0 : posX - textLength + 1;
 
-    if ((posX + textLenght) > cols)
+    if ((posX + textLength) > cols)
         text = text.substring(0, cols - posX);
 
     lcd.setCursor(posX, posY);
